b632a14876c2b3fa35dd0fb27d40cff71cfcfb69
chenping <chenping@daokoudai.com>
2018-08-21 16:23:37 +0800

    ewrwl
    ds

--- wap/suit.py
+++ wap/suit.py
@@ -40,7 +40,7 @@ def suite_stock():
     # suiteTest.addTest(TestWithdraw("test_withdraw_within_availableamt"))
 
     suiteTest.addTest(TestWithdraw("test_commonwithdraw_fail"))
-    suiteTest.addTest(TestWithdraw("test_commonwithdraw"))
+    #suiteTest.addTest(TestWithdraw("test_commonwithdraw"))
 
 
     ##########################投资项目###############################
